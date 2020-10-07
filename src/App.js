1525b6e0364c07f4c07140a5e70f4040da9dcb35
Russ <rperry1886@gmail.com>
2020-10-07 16:24:23 -0400

    Pull out Directory and Menu Items into their own components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
-import HomeaPage from './homepage.component';
 
 function App() {
   return (
     <div>
-      <HomeaPage />
+      <HomePage />
     </div>
   );
 }
