86d54e98ed324f26fe9667c4af273221686b785c
Russ <rperry1886@gmail.com>
2020-10-10 12:21:37 -0400

    Add shop page, collection preview compoenent, and collection item component.

--- src/components/preview-collection/preview-collection.component.jsx
+++ src/components/preview-collection/preview-collection.component.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component'
 
 import './preview-collection.styles.scss';
 
@@ -8,8 +9,8 @@ const PreviewCollection = ({title, items}) => (
         <div className='preview'>
             {items
                 .filter((item, idx) => idx < 4)
-                .map((item) => (
-                    <div key={item.id}>{item.name}</div>
+                .map(({id, ...otherItemProps}) => (
+                    <CollectionItem key={id} {...otherItemProps}/>
                 ))}
         </div>
     </div>
